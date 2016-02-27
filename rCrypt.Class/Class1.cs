6edfdad0a08da6aec4fdb411432f2a18ae736b15
Ruben Gasda <r.gasda@ruerte.de>
2016-02-27 12:48:43 +0100

    rCrypt Initial

---
+++ rCrypt.Class/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rCrypt.Class
+{
+    public class Class1
+    {
+    }
+}
---
+++ rCrypt.From/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace rCryptForm
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                rConsole.Visible(false);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new mainForm());
+            }
+            else
+            {
+                rConsole.s.initialize(args);
+            }
+        }
+    }
+}
